ca47d3a8051fb99139fbc253249359be1614b988
Bashir Hossain <74961832+iambashir@users.noreply.github.com>
2023-05-22 23:18:11 +0600

    First Commit

--- src/main/java/net/javaguides/springboot/model/User.java
+++ src/main/java/net/javaguides/springboot/model/User.java
@@ -1,12 +1,11 @@
 package net.javaguides.springboot.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
